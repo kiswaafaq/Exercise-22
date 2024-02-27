a7dbbb1238356295af0ad227440b766b5e5b8568
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-26 22:55:40 -0800

    first commit

---
+++ intentional_error.js
@@ -0,0 +1,7 @@
+"use strict";
+//Creating Array
+let newArray = ['Will Smith', 'Tom Cruise', 'James Bond'];
+// Accessing array by index
+console.log(newArray[5]);
+// Correcting error
+console.log(newArray);
---
+++ intentional_error.ts
@@ -0,0 +1,9 @@
+
+//Creating Array
+let newArray: string[] = ['Will Smith','Tom Cruise','James Bond'];
+
+// Accessing array by index
+console.log(newArray[5]);
+
+// Correcting error
+console.log(newArray);
